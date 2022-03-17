0e598b3a0dca128612c4626a333fcf0231dddd21
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-03-17 13:39:45 +0000

    [Update]bookのindexページ作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,31 @@
 class BooksController < ApplicationController
-  
+
   def new
-    @book = new
+
   end
-  
+
   def index
+    @book = Book.new
+    @books = Book.all
   end
-  
+
+  def create
+    @book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+
   def show
+    @book = Book.find(oarams[id])
   end
-  
+
   def edit
-end
+  end
+
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,15 @@
 class UsersController < ApplicationController
 
   def show
-     @user = User.find(params[:id])
+    @user = User.find(params[:id])
   end
 
   def index
+    @user = User.find(params[:id])
+    
   end
 
   def edit
+    
   end
 end
