149a2307a510c1205919ce29bf48444d0fc6ddf6
natsumikansatoko <99463494+natsumikansatoko@users.noreply.github.com>
2022-03-21 12:54:49 +0000

    [Update]エラーの解消

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,18 +22,23 @@ class UsersController < ApplicationController
     @books = Book.all
     @book = Book.new
     @users = User.all
-    @user = current_user
+    @user = User.find(current_user.id)
   end
 
   def edit
     @user = User.find(params[:id])
+    if @user == current_user
+      render "edit"
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "You have updated user successfully."
-      redirect_to user_path(@user.id)
+      redirect_to user_path(current_user)
     else
         render :edit
     end
